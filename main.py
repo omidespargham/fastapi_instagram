3be12fcdd561d485f14cf330841d88001ed9a7ae
omidespargham <omidespargham2001@gmail.com>
2023-02-25 18:14:34 +0330

    upload file and see it with mount , and handle it with static files

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from db.database import engine
 # we import it from model because some tables extends from the Base
 from db.models import Base
-from routers import user,post
+from routers import user, post
 from auth import authentication
+# this should be mount in main file not routers
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
 
+app.mount("/files", StaticFiles(directory="uploaded_files"),
+          name="files")
+# app.mount()
 Base.metadata.create_all(engine)
 app.include_router(user.router)
 app.include_router(post.router)
--- routers/post.py
+++ routers/post.py
@@ -6,16 +6,20 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from db import post_db
 from auth.oauth2 import oauth_scheme
+import shutil
+
 
 router = APIRouter(prefix="/post",tags=["post"])
+
+
 
 IMGAE_URL_TYPES = [
     "url",
     "uploaded"
 ]
-
+# ,token:str=Depends(oauth_scheme)
 @router.post("/create_post",response_model=schema.PostShow)
-def create_post(request:schema.PostBase,db:Session= Depends(get_db),token:str=Depends(oauth_scheme)):
+def create_post(request:schema.PostBase,db:Session= Depends(get_db)):
     if request.image_url_type not in IMGAE_URL_TYPES:
         raise HTTPException(status_code=404,detail="this url type is not correct !")
     return post_db.create_post(request,db)
@@ -28,6 +32,10 @@ def get_all_posts(db:Session = Depends(get_db)):
 
 
 
-
-# def upload_file(file:UploadFile=File(...)):
-#     with open
+@router.post("/upload_file")
+def upload_file(file:UploadFile=File(...)):
+    path_file = f"uploaded_files/{file.filename}"
+    with open(path_file,"w+b") as buffer:
+        shutil.copyfileobj(file.file,buffer)
+    
+    return {"path_file":path_file}
